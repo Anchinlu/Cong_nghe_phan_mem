1b2b29e514613e8291abc3f1614c26066974ecc1
huynhquocbaoo <110122035@st.tvu.edu.vn>
2025-06-21 22:11:52 +0700

    feat: setup Docker environment and initial movies module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,30 @@
+// backend/src/app.module.ts
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      // Thêm dấu '!' để báo cho TypeScript rằng chúng ta chắc chắn biến này tồn tại
+      host: process.env.DATABASE_HOST!,
+      port: parseInt(process.env.DATABASE_PORT!, 10),
+      username: process.env.DATABASE_USERNAME!,
+      password: process.env.DATABASE_PASSWORD!, // <-- Lỗi của bạn ở đây đã được sửa
+      database: process.env.DATABASE_NAME!,
+      autoLoadEntities: true,
+      synchronize: true, 
+    }),
+
+    MoviesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
