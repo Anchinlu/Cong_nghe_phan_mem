627cf8c74ffc4f1257a32493f989f8b60d99fcfa
HuynhPhamNhatAn <dh2594182@gmail.com>
2025-07-17 17:20:39 +0700

    sửa lại cấu hình

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000', 
+      'https://cong-nghe-phan-mem.vercel.app' 
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('CineBooking API')
